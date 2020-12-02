8eebfd68195fc0625eee0c96493d7bec883f502e
Maxwell Broom <maximusbroomhandle@gmail.com>
2020-12-01 21:43:22 -0500

    Inital commit of frontend layout. Working to create an example component that can be used for reference later. In the process of reading documentation in order to do so.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import 'fontsource-roboto';
 import Button from '@material-ui/core/Button';
-import SignIn from './SignIn';
+import Main from './Components/Main';
 
 function App() {
   return (
     <div>
-      <SignIn></SignIn>
+      <Main></Main>
     </div>
   );
 }
