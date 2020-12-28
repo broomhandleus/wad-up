c658411223a302fabc9d3e6713beda998a9d87f0
Maxwell Broom <maxwellbroom@pop-os.localdomain>
2020-12-28 16:44:20 -0500

    Made progress on zooming into states on click. Need to figure out why center and zoom aren't updating. Probably need to find a way to fit bounds instead of just changing center and zoom.

--- src/Components/MapView.tsx
+++ src/Components/MapView.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
-import {event} from '../types';
+import { event } from '../types';
+import { Layer, LeafletMouseEventHandlerFn } from 'leaflet';
+import { Feature, Geometry } from 'geojson';
 
 const state_outlines = require("../mapData/state_outlines");
 
@@ -22,8 +24,8 @@ interface props {
 export default function MapView(props: props) {
   const classes = useStyles();
   const events = props.events;
-  let current_center: any = [39.8283, -98.5795]; // had to use any type here? gotta be a smarter way
-  let zoomLevel: number = 5;
+  const [center, setCenter] = useState<[number, number]>([39.8283, -98.5795]);
+  const [zoomLevel, setZoomLevel] = useState(5);
 
   const eventMarkers = events.map((e) =>
     <Marker key={e._id} position={[e.location.latitude, e.location.longitude]} riseOnHover={true}>
@@ -33,23 +35,30 @@ export default function MapView(props: props) {
     </Marker>
   )
 
-  const zoomToFeature = (feature: any) => {
-    console.log("Clicked on feature: " + JSON.stringify(feature.properties.NAME));
+  // Adds the ability to zoom to each state area when clicked
+  const zoomToFeature: LeafletMouseEventHandlerFn = (e) => {
+    const newCenterLat: number = (e.target._bounds._southWest.lat + e.target._bounds._northEast.lat) / 2;
+    const newCenterLong: number = (e.target._bounds._southWest.lng + e.target._bounds._northEast.lng) / 2;
+    console.log("New Center: " + newCenterLat + ", " + newCenterLong);
+    setCenter([newCenterLat, newCenterLong]);
+    setZoomLevel(7);
+    console.log("Center: " + center);
+    console.log("Zoom: " + zoomLevel);
   }
-
-  const onEachFeature = (feature: any, layer: any) => {
+  const onEachFeature = (feature: Feature<Geometry, any>, layer: Layer) => {
     layer.on({
-      click: zoomToFeature(feature)
+      click: zoomToFeature
     });
   }
 
   // TODO: use the Marker/Popup elements from react-leaflet to show events!
+  // TODO: set color of the marker based on the event category like we did with the card border
   // TODO: implement the "set default" function so a user can set their preferred start area
   // Using the tile layer from open street maps as dictated in the react-leaflet docs
   return (
     <MapContainer
       className={classes.mapView}
-      center={current_center}
+      center={center}
       zoom={zoomLevel}
     >
       <TileLayer
